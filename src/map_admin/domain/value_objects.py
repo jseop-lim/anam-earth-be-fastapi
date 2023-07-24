77745d9148317d1bf65704a040851faa97815018
Jeongseop Lim <jseoplim@gmail.com>
2023-07-24 21:47:48 +0900

    feat: Define value objects

---
+++ src/map_admin/domain/value_objects.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from decimal import Decimal
+from enum import StrEnum
+
+
+@dataclass(frozen=True, kw_only=True)
+class Point:
+    longitude: Decimal
+    latitude: Decimal
+
+
+class RoadQuality(StrEnum):
+    HIGH = '상'
+    MEDIUM = '중'
+    LOW = '하'
