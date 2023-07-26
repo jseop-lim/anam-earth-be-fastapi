1747d92d437cd3570a897b3b74344e32c63327db
Jeongseop Lim <jseoplim@gmail.com>
2023-07-26 21:24:15 +0900

    refactor: Add options to dataclass

--- src/map_admin/application/dtos.py
+++ src/map_admin/application/dtos.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from decimal import Decimal
 
 
-@dataclass
+@dataclass(frozen=True, kw_only=True)
 class ListNodesOutputData:
     name: str
     longitude: Decimal
