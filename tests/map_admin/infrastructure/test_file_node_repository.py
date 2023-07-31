b6aed6e1a433ee5da07a287bded8b76fb60533ea
Jeongseop Lim <jseoplim@gmail.com>
2023-07-31 22:33:21 +0900

    refactor: Seperate file fixture from tests

--- tests/map_admin/infrastructure/test_file_node_repository.py
+++ tests/map_admin/infrastructure/test_file_node_repository.py
@@ -2,24 +2,40 @@ import json
 import os
 from decimal import Decimal
 from tempfile import NamedTemporaryFile
+from typing import Generator
+
+import pytest
 
 from map_admin.domain.entities import Node
 from map_admin.domain.value_objects import Point
 from map_admin.infrastructure.repositories import FileNodeRepository
 
 
-def test_get_all_nodes() -> None:
-    nodes = [
+@pytest.fixture()
+def temp_file_path() -> Generator[str, None, None]:
+    with NamedTemporaryFile(mode='w', delete=False) as file:
+        json.dump([], file)
+        file_path: str = file.name
+
+    yield file_path
+
+    # cleanup after test
+    os.unlink(file_path)
+
+
+def test_get_all_nodes(
+    temp_file_path: str,
+) -> None:
+    nodes: list[dict[str, str]] = [
         {'name': 'Node 1', 'longitude': '1.0', 'latitude': '2.0'},
         {'name': 'Node 2', 'longitude': '3.0', 'latitude': '4.0'},
     ]
-    with NamedTemporaryFile(mode='w', delete=False) as file:
+    with open(temp_file_path, 'w') as file:
         json.dump(nodes, file)
-        file_path = file.name
 
-    repo = FileNodeRepository(file_path=file_path)
+    node_repo = FileNodeRepository(file_path=temp_file_path)
+    result = node_repo.get_all_nodes()
 
-    result = repo.get_all_nodes()
     assert result == [
         Node(
             name='Node 1',
@@ -37,19 +53,10 @@ def test_get_all_nodes() -> None:
         ),
     ]
 
-    os.unlink(file_path)
 
-
-def test_create_node() -> None:
-    nodes = [
-        {'name': 'Node 1', 'longitude': '1.0', 'latitude': '2.0'},
-        {'name': 'Node 2', 'longitude': '3.0', 'latitude': '4.0'},
-    ]
-    with NamedTemporaryFile(mode='w', delete=False) as file:
-        json.dump(nodes, file)
-        file_path = file.name
-
-    repo = FileNodeRepository(file_path=file_path)
+def test_create_node(
+    temp_file_path: str,
+) -> None:
     new_node = Node(
         name='Node 3',
         point=Point(
@@ -58,10 +65,9 @@ def test_create_node() -> None:
         ),
     )
 
-    repo.create_node(node=new_node)
+    node_repo = FileNodeRepository(file_path=temp_file_path)
+    node_repo.create_node(node=new_node)
 
-    with open(file_path, 'r') as file:
+    with open(temp_file_path, 'r') as file:
         result = json.load(file)
     assert result[-1] == {'name': 'Node 3', 'longitude': '5.0', 'latitude': '6.0'}
-
-    os.unlink(file_path)
