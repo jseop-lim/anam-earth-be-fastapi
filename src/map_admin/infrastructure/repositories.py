1b2f8dd97d529b6e0ff5849cab93874fe4d46638
Jeongseop Lim <jseoplim@gmail.com>
2023-07-31 22:08:11 +0900

    feat: Make file node repository

--- src/map_admin/infrastructure/repositories.py
+++ src/map_admin/infrastructure/repositories.py
@@ -1,4 +1,6 @@
+import json
 from decimal import Decimal
+from typing import TypedDict
 
 from map_admin.application.repositories import NodeRepository
 from map_admin.domain.entities import Node
@@ -14,3 +16,47 @@ class FakeNodeRepository(NodeRepository):
 
     def create_node(self, node: Node) -> None:
         print(f'Create node: {node}')
+
+
+class FileNode(TypedDict):
+    name: str
+    longitude: str
+    latitude: str
+
+
+class FileNodeRepository(NodeRepository):
+    def __init__(
+        self,
+        file_path: str,
+    ) -> None:
+        self.file_path = file_path
+
+    def get_all_nodes(self) -> list[Node]:
+        with open(self.file_path, 'r') as file:
+            nodes: list[FileNode] = json.load(file)
+
+        return [
+            Node(
+                name=node['name'],
+                point=Point(
+                    longitude=Decimal(node['longitude']),
+                    latitude=Decimal(node['latitude']),
+                ),
+            )
+            for node in nodes
+        ]
+
+    def create_node(self, node: Node) -> None:
+        with open(self.file_path, 'r') as file:
+            nodes: list[FileNode] = json.load(file)
+
+        nodes.append(
+            {
+                'name': node.name,
+                'longitude': str(node.point.longitude),
+                'latitude': str(node.point.latitude),
+            }
+        )
+
+        with open(self.file_path, 'w') as file:
+            json.dump(nodes, file, indent=4)
