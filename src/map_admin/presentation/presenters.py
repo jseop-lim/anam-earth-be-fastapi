4171ba5975aed307aeec9215043815199682df6d
Jeongseop Lim <jseoplim@gmail.com>
2023-08-09 21:59:52 +0900

    refactor: Redefine view model as type alias

--- src/map_admin/presentation/presenters.py
+++ src/map_admin/presentation/presenters.py
@@ -1,19 +1,24 @@
+from typing import TypeAlias
+
 from typing_extensions import TypedDict
 
 from map_admin.application.boundaries import ListNodesOutputBoundary
 from map_admin.application.dtos import ListNodesOutputData
 
 
-class ListNodesJsonViewModel(TypedDict):
+class NodeJsonViewModel(TypedDict):
     name: str
     longitude: str
     latitude: str
 
 
+ListNodesJsonViewModel: TypeAlias = list[NodeJsonViewModel]
+
+
 class ListNodesJsonPresenter(ListNodesOutputBoundary):
     def present(self, output_data_list: list[ListNodesOutputData]) -> None:
-        self._view_model: list[ListNodesJsonViewModel] = [
-            ListNodesJsonViewModel(
+        self._view_model: ListNodesJsonViewModel = [
+            NodeJsonViewModel(
                 name=output_data.name,
                 longitude=f"{output_data.longitude:.6f}",
                 latitude=f"{output_data.latitude:.6f}",
@@ -21,5 +26,5 @@ class ListNodesJsonPresenter(ListNodesOutputBoundary):
             for output_data in output_data_list
         ]
 
-    def get_view_model(self) -> list[ListNodesJsonViewModel]:
+    def get_view_model(self) -> ListNodesJsonViewModel:
         return self._view_model
