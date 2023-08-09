ffde7251e7a576d652fb961e0c030db57c6eb1e0
Jeongseop Lim <jseoplim@gmail.com>
2023-08-09 22:06:34 +0900

    chore: Add dependency to pre-commit mypy

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+from containers import Container
+from map_admin.presentation import apis as map_admin_apis
+
+container = Container()
+container.wire(
+    modules=[
+        map_admin_apis,
+    ],
+)
+
+app = FastAPI()
+app.include_router(map_admin_apis.router)
