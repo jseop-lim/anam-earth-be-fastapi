f93d546421019fb7288eb83251c7ad603c7a3b19
Jeongseop Lim <jseoplim@gmail.com>
2023-08-10 20:13:49 +0900

    feat: Make main module

--- src/main.py
+++ src/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from containers import Container
 from map_admin.presentation import apis as map_admin_apis
+from settings import Settings
 
 container = Container()
+container.config.from_dict(Settings().model_dump())
 container.wire(
     modules=[
         map_admin_apis,
