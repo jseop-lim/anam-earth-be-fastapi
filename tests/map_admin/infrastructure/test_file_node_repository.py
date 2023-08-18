a33ed772ebab3eb9abdd8a4a8c3eae8d2d690d30
Jeongseop Lim <jseoplim@gmail.com>
2023-08-19 00:05:33 +0900

    test: Add id field to file node repository

--- tests/map_admin/infrastructure/test_file_node_repository.py
+++ tests/map_admin/infrastructure/test_file_node_repository.py
@@ -8,7 +8,7 @@ import pytest
 
 from map_admin.domain.entities import Node
 from map_admin.domain.value_objects import Point
-from map_admin.infrastructure.repositories import FileNodeRepository
+from map_admin.infrastructure.repositories import FileNode, FileNodeRepository
 
 
 @pytest.fixture()
@@ -23,12 +23,42 @@ def temp_file_path() -> Generator[str, None, None]:
     os.unlink(file_path)
 
 
+@pytest.mark.parametrize(
+    "nodes, expected_next_id",
+    [
+        (
+            [],
+            1,
+        ),
+        (
+            [
+                {"id": 1, "name": "Node 1", "longitude": "1.0", "latitude": "2.0"},
+                {"id": 3, "name": "Node 3", "longitude": "3.0", "latitude": "4.0"},
+            ],
+            4,
+        ),
+    ],
+)
+def test_get_next_id(
+    temp_file_path: str,
+    nodes: list[FileNode],
+    expected_next_id: int,
+) -> None:
+    with open(temp_file_path, "w") as file:
+        json.dump(nodes, file)
+
+    node_repo = FileNodeRepository(file_path=temp_file_path)
+    result = node_repo.get_next_id()
+
+    assert result == expected_next_id
+
+
 def test_get_all_nodes(
     temp_file_path: str,
 ) -> None:
-    nodes: list[dict[str, str]] = [
-        {"name": "Node 1", "longitude": "1.0", "latitude": "2.0"},
-        {"name": "Node 2", "longitude": "3.0", "latitude": "4.0"},
+    nodes: list[FileNode] = [
+        {"id": 1, "name": "Node 1", "longitude": "1.0", "latitude": "2.0"},
+        {"id": 2, "name": "Node 2", "longitude": "3.0", "latitude": "4.0"},
     ]
     with open(temp_file_path, "w") as file:
         json.dump(nodes, file)
@@ -38,6 +68,7 @@ def test_get_all_nodes(
 
     assert result == [
         Node(
+            id=1,
             name="Node 1",
             point=Point(
                 longitude=Decimal("1.0"),
@@ -45,6 +76,7 @@ def test_get_all_nodes(
             ),
         ),
         Node(
+            id=2,
             name="Node 2",
             point=Point(
                 longitude=Decimal("3.0"),
@@ -58,6 +90,7 @@ def test_create_node(
     temp_file_path: str,
 ) -> None:
     new_node = Node(
+        id=3,
         name="Node 3",
         point=Point(
             longitude=Decimal("5.0"),
@@ -69,5 +102,10 @@ def test_create_node(
     node_repo.create_node(node=new_node)
 
     with open(temp_file_path, "r") as file:
-        result = json.load(file)
-    assert result[-1] == {"name": "Node 3", "longitude": "5.0", "latitude": "6.0"}
+        result: list[FileNode] = json.load(file)
+    assert result[-1] == {
+        "id": 3,
+        "name": "Node 3",
+        "longitude": "5.0",
+        "latitude": "6.0",
+    }
