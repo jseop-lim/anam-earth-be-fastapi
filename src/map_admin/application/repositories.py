b20b3fbb2d81bc1102bdca5e465898fa09502055
Jeongseop Lim <jseoplim@gmail.com>
2023-08-21 22:45:21 +0900

    feat: Add methods and class to node repository

--- src/map_admin/application/repositories.py
+++ src/map_admin/application/repositories.py
@@ -13,5 +13,16 @@ class NodeRepository(ABC):
         raise NotImplementedError
 
     @abstractmethod
+    def get_node_by_id(self, node_id: int) -> Node:
+        raise NotImplementedError
+
+    @abstractmethod
     def create_node(self, node: Node) -> None:
         raise NotImplementedError
+
+    @abstractmethod
+    def update_node(self, node: Node) -> None:
+        raise NotImplementedError
+
+    class NodeNotFoundError(Exception):
+        """노드를 찾지 못할 때 발생하는 에러"""
