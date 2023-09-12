a329ad83f8d5012778f2a3ee19031bc2cb2085ac
Jeongseop Lim <jseoplim@gmail.com>
2023-09-12 16:32:22 +0900

    feat: Define magic methods in Edge entity

--- src/map_admin/domain/entities.py
+++ src/map_admin/domain/entities.py
@@ -26,3 +26,12 @@ class Edge:
     is_stair: bool
     is_step: bool
     quality: RoadQuality
+
+    def __post_init__(self) -> None:
+        if self.nodes[0] == self.nodes[1]:
+            raise ValueError("Edge cannot connect to itself")
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Edge):
+            return NotImplemented
+        return self.nodes == other.nodes or self.nodes == tuple(reversed(other.nodes))
