0e6273ae2d14a2044390e143bee76f37a84fcce6
Jeongseop Lim <jseoplim@gmail.com>
2023-09-12 16:33:17 +0900

    feat: Define add_edge method to Node entity

--- src/map_admin/domain/entities.py
+++ src/map_admin/domain/entities.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from decimal import Decimal
+from typing import Self
 
 from map_admin.domain.value_objects import Point, RoadQuality
 
@@ -16,6 +17,32 @@ class Node:
 
     def update_point(self, point: Point) -> None:
         self.point = point
+
+    def add_edge(
+        self,
+        other_node: Self,
+        vertical_distance: Decimal,
+        horizontal_distance: Decimal,
+        is_stair: bool,
+        is_step: bool,
+        quality: RoadQuality,
+    ) -> None:
+        if self == other_node:
+            raise ValueError("Edge cannot connect to itself")
+
+        if any(other_node in edge.nodes for edge in self.edges):
+            raise ValueError("Edge already exists")
+
+        edge = Edge(
+            nodes=(self, other_node),
+            vertical_distance=vertical_distance,
+            horizontal_distance=horizontal_distance,
+            is_stair=is_stair,
+            is_step=is_step,
+            quality=quality,
+        )
+        self.edges.add(edge)
+        other_node.edges.add(edge)
 
 
 @dataclass(kw_only=True)
