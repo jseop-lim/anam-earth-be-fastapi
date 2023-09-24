74aa4bbe23fa872f03cf49f528f19bdf3df69d81
Jeongseop Lim <jseoplim@gmail.com>
2023-09-24 17:12:00 +0900

    test: Add test for comparing node equal

--- tests/map_admin/domain/test_node.py
+++ tests/map_admin/domain/test_node.py
@@ -1,7 +1,41 @@
 from decimal import Decimal
 
-from map_admin.domain.entities import Node
-from map_admin.domain.value_objects import Point
+from map_admin.domain.entities import Edge, Node
+from map_admin.domain.value_objects import Point, RoadQuality
+
+
+def test_eq() -> None:
+    nodes: dict[int, Node] = {
+        1: Node(
+            id=1,
+            name="Node 1",
+            point=Point(
+                longitude=Decimal("1.0"),
+                latitude=Decimal("2.0"),
+            ),
+            edges=[],
+        ),
+        2: Node(
+            id=1,
+            name="Node 1",
+            point=Point(
+                longitude=Decimal("1.0"),
+                latitude=Decimal("2.0"),
+            ),
+            edges=[
+                Edge(
+                    node_ids=(1, 2),
+                    vertical_distance=Decimal("1.0"),
+                    horizontal_distance=Decimal("2.0"),
+                    is_stair=False,
+                    is_step=False,
+                    quality=RoadQuality.HIGH,
+                ),
+            ],
+        ),
+    }
+
+    assert nodes[1] == nodes[2]
 
 
 def test_update_name() -> None:
