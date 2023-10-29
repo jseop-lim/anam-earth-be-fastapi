4aa4a88501b0c13e1000de5120926ac5730b0b0c
Jeongseop Lim <jseoplim@gmail.com>
2023-10-29 14:31:17 +0900

    test: Add Edge update methods test case

--- tests/map_admin/domain/test_edge.py
+++ tests/map_admin/domain/test_edge.py
@@ -40,3 +40,78 @@ def test_eq() -> None:
     }
 
     assert edges[1] == edges[2]
+
+
+def test_update_vertical_distance() -> None:
+    edge = Edge(
+        node_ids=(1, 2),
+        vertical_distance=Decimal("1.0"),
+        horizontal_distance=Decimal("2.0"),
+        is_stair=False,
+        is_step=False,
+        quality=RoadQuality.HIGH,
+    )
+
+    edge.update_vertical_distance(vertical_distance=Decimal("3.0"))
+
+    assert edge.vertical_distance == Decimal("3.0")
+
+
+def test_update_horizontal_distance() -> None:
+    edge = Edge(
+        node_ids=(1, 2),
+        vertical_distance=Decimal("1.0"),
+        horizontal_distance=Decimal("2.0"),
+        is_stair=False,
+        is_step=False,
+        quality=RoadQuality.HIGH,
+    )
+
+    edge.update_horizontal_distance(horizontal_distance=Decimal("4.0"))
+
+    assert edge.horizontal_distance == Decimal("4.0")
+
+
+def test_update_is_stair() -> None:
+    edge = Edge(
+        node_ids=(1, 2),
+        vertical_distance=Decimal("1.0"),
+        horizontal_distance=Decimal("2.0"),
+        is_stair=False,
+        is_step=False,
+        quality=RoadQuality.HIGH,
+    )
+
+    edge.update_is_stair(is_stair=True)
+
+    assert edge.is_stair is True
+
+
+def test_update_is_step() -> None:
+    edge = Edge(
+        node_ids=(1, 2),
+        vertical_distance=Decimal("1.0"),
+        horizontal_distance=Decimal("2.0"),
+        is_stair=False,
+        is_step=False,
+        quality=RoadQuality.HIGH,
+    )
+
+    edge.update_is_step(is_step=True)
+
+    assert edge.is_step is True
+
+
+def test_update_quality() -> None:
+    edge = Edge(
+        node_ids=(1, 2),
+        vertical_distance=Decimal("1.0"),
+        horizontal_distance=Decimal("2.0"),
+        is_stair=False,
+        is_step=False,
+        quality=RoadQuality.HIGH,
+    )
+
+    edge.update_quality(quality=RoadQuality.LOW)
+
+    assert edge.quality == RoadQuality.LOW
